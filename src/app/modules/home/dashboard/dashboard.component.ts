05790e6f5d96b8a32b8731c2e95e54e52bea36d9
hmehta20 <mehtahelly20@gmail.com>
2023-05-28 23:20:19 +0530

    fix dashboard data and confirmation popup

--- src/app/modules/home/dashboard/dashboard.component.ts
+++ src/app/modules/home/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { CommonDialogComponent } from '../../shared/components/common-dialog/common-dialog.component';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -11,19 +11,20 @@ export interface RideObject {
   noOfRiders: number;
   noOfBuses: number;
   details: string;
+  status: number;
 }
 
 const ELEMENT_DATA: RideObject[] = [
-  { noOfRiders: 1, name: 'Hydrogen', noOfBuses: 1.0079, details: 'H' },
-  { noOfRiders: 2, name: 'Helium', noOfBuses: 4.0026, details: 'He' },
-  { noOfRiders: 3, name: 'Lithium', noOfBuses: 6.941, details: 'Li' },
-  { noOfRiders: 4, name: 'Beryllium', noOfBuses: 9.0122, details: 'Be' },
-  { noOfRiders: 5, name: 'Boron', noOfBuses: 10.811, details: 'B' },
-  { noOfRiders: 6, name: 'Carbon', noOfBuses: 12.0107, details: 'C' },
-  { noOfRiders: 7, name: 'Nitrogen', noOfBuses: 14.0067, details: 'N' },
-  { noOfRiders: 8, name: 'Oxygen', noOfBuses: 15.9994, details: 'O' },
-  { noOfRiders: 9, name: 'Fluorine', noOfBuses: 18.9984, details: 'F' },
-  { noOfRiders: 10, name: 'Neon', noOfBuses: 20.1797, details: 'Ne' },
+  { status:1, noOfRiders: 1, name: 'Hydrogen', noOfBuses: 1.0079, details: 'H' },
+  { status:2, noOfRiders: 2, name: 'Helium', noOfBuses: 4.0026, details: 'He' },
+  { status:3, noOfRiders: 3, name: 'Lithium', noOfBuses: 6.941, details: 'Li' },
+  { status:1, noOfRiders: 4, name: 'Beryllium', noOfBuses: 9.0122, details: 'Be' },
+  { status:3, noOfRiders: 5, name: 'Boron', noOfBuses: 10.811, details: 'B' },
+  { status:1, noOfRiders: 6, name: 'Carbon', noOfBuses: 12.0107, details: 'C' },
+  { status:2, noOfRiders: 7, name: 'Nitrogen', noOfBuses: 14.0067, details: 'N' },
+  { status:1, noOfRiders: 8, name: 'Oxygen', noOfBuses: 15.9994, details: 'O' },
+  { status:1, noOfRiders: 9, name: 'Fluorine', noOfBuses: 18.9984, details: 'F' },
+  { status:2, noOfRiders: 10, name: 'Neon', noOfBuses: 20.1797, details: 'Ne' },
 ];
 
 @Component({
@@ -31,9 +32,9 @@ const ELEMENT_DATA: RideObject[] = [
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: String[] = [];
+  displayedColumns: String[] = ['rideName', 'noOfBuses', 'noOfRiders', 'details', 'status', 'action'];
   searchTerm = "";
   status = new FormControl('');
 
@@ -54,23 +55,39 @@ export class DashboardComponent implements OnInit {
   dataSource = new MatTableDataSource<RideObject>();
 
   @ViewChild(MatSort) sort = new MatSort;
-  // @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);;
   @ViewChild("table") table: any;
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.displayedColumns = ['rideName', 'noOfBuses', 'noOfRiders', 'details', 'status', 'action'];
-    // this.dataSource = ELEMENT_DATA;
+    this.dataSource = new MatTableDataSource<RideObject> (ELEMENT_DATA);
   }
 
   ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
   openDialog() {
-    this.dialog.open(CommonDialogComponent);
+    let dialogRef = this.dialog.open(CommonDialogComponent, {
+      panelClass:"confirmation-dialog",
+      data: {
+        title: "Confrmation",
+        description: "ABCD",
+        buttons: [
+          {
+            text: "Cancel",
+            className: "cancel-btn"
+          },
+          {
+            text: "Save",
+            className: "save-btn"
+          }
+        ]
+      }
+    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 
 }
--- src/app/modules/shared/components/common-dialog/common-dialog.component.ts
+++ src/app/modules/shared/components/common-dialog/common-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-common-dialog',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommonDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<CommonDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.data);
+  }
+
+  close(): void {
+    this.dialogRef.close();
   }
 
 }

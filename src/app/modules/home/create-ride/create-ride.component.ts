c80a0599cb00cc53e7f64edde2c889e14f476206
hmehta20 <mehtahelly20@gmail.com>
2023-05-30 02:41:27 +0530

    add create-ride component fix header/navigation menu

--- src/app/modules/home/create-ride/create-ride.component.ts
+++ src/app/modules/home/create-ride/create-ride.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface RiderObject {
+  firstName: string;
+  lastName: string;
+  streetAddress: string;
+  city: string;
+  state: string;
+  zip: number;
+}
 @Component({
   selector: 'app-create-ride',
   templateUrl: './create-ride.component.html',
   styleUrls: ['./create-ride.component.scss']
 })
-export class CreateRideComponent implements OnInit {
+export class CreateRideComponent implements OnInit, AfterViewInit {
+
+  options: string[] = ["a", "b"];
+  pickupControl = new FormControl();
+  destControl = new FormControl();
+
+  displayedColumns: String[] = ['firstName', 'lastName', 'streetAddress', 'city', 'state', 'zip'];
+
+  dataSource = new MatTableDataSource<RiderObject>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);;
+  @ViewChild("table") table: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }

ba4ad84899f5a33e34945dae27c3fc5996d8e9ef
Harshit Shah <shahharshit737@gmail.com>
2023-06-03 20:43:07 +0530

    Login UI

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LandingComponent } from './landing.component';
-import { LandingRoutingModule } from './landing-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { VerifyLoginComponent } from './verify-login/verify-login.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LandingComponent } from "./landing.component";
+import { LandingRoutingModule } from "./landing-routing.module";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { VerifyLoginComponent } from "./verify-login/verify-login.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
     LandingComponent,
     LoginComponent,
     RegisterComponent,
-    VerifyLoginComponent
+    VerifyLoginComponent,
   ],
   imports: [
     CommonModule,
-    LandingRoutingModule
-  ]
+    LandingRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+  ],
 })
-export class LandingModule { }
+export class LandingModule {}
--- src/app/modules/landing/login/login.component.ts
+++ src/app/modules/landing/login/login.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  imageUrl =
+    "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

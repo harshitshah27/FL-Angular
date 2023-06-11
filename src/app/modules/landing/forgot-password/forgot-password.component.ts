542643931beeba038e826aab7dbbb22c736e2112
Harshit Shah <shahharshit737@gmail.com>
2023-06-11 14:52:23 +0530

    forget password

--- src/app/modules/landing/forgot-password/forgot-password.component.ts
+++ src/app/modules/landing/forgot-password/forgot-password.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  selector: "app-forgot-password",
+  templateUrl: "./forgot-password.component.html",
+  styleUrls: ["./forgot-password.component.scss"],
 })
 export class ForgotPasswordComponent {
+  email: string = "";
+  password: string = "";
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  doLogin() {
+    console.log(this.email);
+    console.log(this.password);
+  }
 }
